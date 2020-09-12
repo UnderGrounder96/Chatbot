5575e9e0c25f4e43e7b7698e0a494b77dabcfd8b
Lucio Afonso <lucioafonso@icloud.com>
2020-09-12 18:01:46 +0200

    Using Pytest for unit testing Added pytest.ini Moved source_code to src/ Updated workflow file Updated .gitignore Updated requirements.txt

---
+++ src/nltk_utils.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import nltk
+
+from nltk.stem.porter import PorterStemmer
+
+# nltk.download('punkt')
+
+
+def tokenize(sentence):
+    """
+    split sentence into array of tokens.
+    a token can be a word, a punctuation character, a symbol or number
+    """
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    """
+    stemming is finding the root form of the word
+    examples:
+    words = ["organize", "organizes", "organizing"]
+    [stem(w) for w in words] -> ["organ", "organ", "organ"]
+    """
+    return PorterStemmer().stem(word.lower())
+
+
+def bag_of_words(tokenized_sentence, words):
+    pass
---
+++ tests/test_nltk_utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from src.nltk_utils import *
+
+class TestNltkUtils:
+
+  def test_tokenize(self):
+      result = tokenize("What's on the menu?")
+      assert type(result) is list
+      assert len(result) == 6
+
+
+  def test_stem(self):
+      result = [stem(w) for w in ["organize", "organizes", "organizing"]]
+      assert "organ" in result
+      assert len(result) == 3
+
+
+  def test_bag_of_words(self):
+      pass
